3b517098763b2d67489212ab7e56f1e60d6bd84a
Nattie <natalie.brooks3@gmail.com>
2017-08-09 17:11:25 -0400

    Added a fleet of aliens, added ship lives

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.sprite import Group
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship 
 import game_functions as gf
 
@@ -10,24 +11,37 @@ import game_functions as gf
 def run_game():
     # Initilize pygame, settings, and screen object
     pygame.init()
+
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+
+    # Create an isntnace to store the game statistics
+    stats = GameStats(ai_settings)
+
     # Make a ship.
     ship = Ship(ai_settings, screen)
     # Make a group to store bullets.
     bullets = Group()
+    # Group to store Aliens
+    aliens = Group()
 
-    #Start themain loop for the game.
+    # Make an alien fleet
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    #Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events
 
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets) 
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets) 
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 
--- ship.py
+++ ship.py
@@ -30,6 +30,10 @@ class Ship():
         if self.moving_left and self.rect.left > 0:
             self.rect.centerx -= self.ai_settings.ship_speed_factor
 
+    def center_ship(self):
+        """ Center the ship on the screen """
+        self.center = self.screen_rect.centerx
+
     def blitme(self):
         """Draw the ship at its current location """
         self.screen.blit(self.image, self.rect)
